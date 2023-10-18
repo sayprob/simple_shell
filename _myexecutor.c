ab5956afee0a37aa0affcc7e1bec9545ca6f2888
KingsleyOC <135044415+KingsleyOC@users.noreply.github.com>
2023-10-18 21:38:35 +0000

    Fix Exit Edge Case

--- _myexecutor.c
+++ _myexecutor.c
@@ -6,14 +6,15 @@
  * @argv: filename
  * @count: loop count
  */
-void execute_input(const char *input, char *argv, int count)
+void execute_input(char *input, char *argv, int count)
 {
 	int i;
 	char **arg_str, *path_cmd;
 
 	if (cust_strncmp(input, "exit", 4) == 0)
 	{
-		exit_execute(input);
+		free(input);
+		exit(0);
 	}
 	arg_str = tokenize_input(input);
 	if (!arg_str[0])
@@ -44,16 +45,4 @@ void execute_input(const char *input, char *argv, int count)
 	for (i = 0; arg_str && arg_str[i] != NULL; i++)
 		free(arg_str[i]);
 	free(arg_str);
-}
-
-/**
- * exit_execute - Checks the user
- * input to see if it matche the "exit" comman
- *
- * @input: The user's input string
- */
-void exit_execute(const char *input)
-{
-	if (cust_strlen(input) == 4)
-		exit(EXIT_SUCCESS);
 }
